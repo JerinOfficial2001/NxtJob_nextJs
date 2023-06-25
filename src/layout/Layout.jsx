39e6b6ae08860df930cf488ea766ed806d4d0f48
jerin2001 <jerinjerijree@gmail.com>
2023-06-25 16:30:42 +0530

    deployment

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -14,13 +14,10 @@ export default function Layout({
   setopenMenu,
 }) {
   return (
-    <div
-      className=" w-[100vw] h-[100vh] flex flex-col gap-5 bg-[#F5F5F5] max-[1000px]:h-[100vh] max-[1000px]:w-[100%] relative"
-     
-    >
+    <div className=" w-[100vw] h-[100vh] flex flex-col gap-5 bg-[#F5F5F5] max-[1000px]:h-[100vh] max-[1000px]:w-[100vw] relative">
       <Toaster position="top-center" />
       <TopBar_layout />
-      <div className=" flex gap-5 px-4 max-[1000px]:w-[100%]">
+      <div className=" flex gap-5 px-4 max-[1000px]:w-[100%] max-[1000px]:h-[100%]">
         <div className="h-fit  w-[250px] py-5 rounded-md bg-white ml-2  max-[1000px]:hidden">
           <SideBar
             SIDEBAR_ITEMS={SIDEBAR_ITEMS}
@@ -31,7 +28,7 @@ export default function Layout({
 
         <div className="w-[82%] h-[100%] flex flex-col gap-4  max-[1000px]:w-[100%] max-[1000px]:h-[100%] relative">
           {open && (
-            <div className="w-[20%] absolute top-20 bg-white h-[75%] rounded-xl z-[10] min-[1000px]:hidden">
+            <div className="w-[100%] absolute top-20 bg-white h-[600px] rounded-xl z-[10] min-[1000px]:hidden">
               <SideBar
                 setopenMenu={setopenMenu}
                 SIDEBAR_ITEMS={SIDEBAR_ITEMS}
@@ -40,7 +37,9 @@ export default function Layout({
               />
             </div>
           )}
-          <div className="gap-3 flex flex-col h-[100%] w-[100%]">{children}</div>
+          <div className="gap-3 flex flex-col h-[100%] w-[100%]">
+            {children}
+          </div>
         </div>
       </div>
     </div>
